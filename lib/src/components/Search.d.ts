430e4c45aeb0f918af1ab455689fcc069fce2559
davecourtois <dave.courtois60@gmail.com>
2020-11-11 16:52:21 -0500

    remove unwanted sturff

--- lib/src/components/Search.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Search Box
- */
-export class SearchBar extends HTMLElement {
-    connectedCallback(): void;
-    search(query: any, paths: any, fields: any, language: any, offset: any, pageSize: any, snippetLenght: any): void;
-}
