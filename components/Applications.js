a2b08f0525815ce12f62b253a29d893e2b1c91a7
davecourtois <dave.courtois60@gmail.com>
2021-03-17 15:01:00 -0400

    application icons webcomponent...

--- components/Applications.js
+++ components/Applications.js
@@ -27,133 +27,172 @@ export class ApplicationsMenu extends Menu {
     // Create the applicaiton view.
     constructor() {
         super("applications", "apps", "Applications")
-    }
-
-    init() {
         let html = `
             <style>
-                
+                                
+                #applications_menu_div{
+                    background-color: var(--palette-background-paper);
+                }
+
                 #applications-div {
                     display: flex;
                     flex-wrap: wrap;
                     padding: 10px;
                     width: 300px;
+                    height: 100%;
                 }
+
             </style>
             <div id="applications-div">
-                
+                <globular-applications-panel id="application-panel-toolbar-menu"></globular-applications-panel>
             </div>
         `
+
+        this.shadowRoot.appendChild(this.getMenuDiv())
+
         let range = document.createRange()
         this.getMenuDiv().innerHTML = "" // remove existing elements.
         this.getMenuDiv().appendChild(range.createContextualFragment(html));
-
-        this.getMenuDiv().appendChild(range.createContextualFragment(html));
         this.getMenuDiv().style.height = "380px";
         this.getMenuDiv().style.overflowY = "auto";
+    }
+
+    init() {
 
         this.shadowRoot.appendChild(this.getMenuDiv())
+
         // Action's
-        let div = this.shadowRoot.getElementById("applications-div")
+        this.getMenuDiv().querySelector("#application-panel-toolbar-menu").init();
+
+        this.shadowRoot.removeChild(this.getMenuDiv())
+    }
+}
+
+customElements.define('globular-applications-menu', ApplicationsMenu)
+
+/**
+ * Login/Register functionality.
+ */
+export class ApplicationsPanel extends HTMLElement {
+    // attributes.
+
+    // Create the applicaiton view.
+    constructor() {
+        super()
+        // Set the shadow dom.
+        this.attachShadow({ mode: 'open' });
+
+        this.shadowRoot.innerHTML = `
+        <style>
+        ${theme}
+            .container {
+                display: flex;
+            }
+
+            .application-div {
+                display: flex;
+                position: relative;
+                flex-direction: column;
+                align-items: center;
+                height: 80px;
+                width: 80px;
+                margin: 5px;
+                padding: 5px;
+                border-radius: 5px;
+                transition: background 0.2s ease,padding 0.8s linear;
+            }
+
+            .application-div img{
+                filter: invert(0%);
+            }
+            .application-div:hover{
+                cursor: pointer;
+                -webkit-filter: invert(10%);
+                filter: invert(10%);
+            }
+
+            .application-div img{
+                height: 56px;
+                width: 56px;
+            }
+
+            .application-div span{
+                margin-top: 5px;
+                color: #404040;
+                display: inline-block;
+                font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
+                font-size: 14px;
+                letter-spacing: .09px;
+                line-height: 16px;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                width: 76px;
+                text-align: center;
+                color: var(--palette-text-primary);
+            }
+
+        </style>
+        <div class="container"></div>
+        `
+    }
+
+
+    // The connection callback.
+    connectedCallback() {
+
+    }
+
+    init() {
+
         Application.getAllApplicationInfo((infos) => {
-            this.shadowRoot.appendChild(this.getMenuDiv())
             let range = document.createRange()
             for (var i = 0; i < infos.length; i++) {
                 let applicaiton = infos[i]
                 let html = `
-                <style>
-                    ${theme}
-                    
-                    #applications_menu_div{
-                        background-color: var(--palette-background-paper);
-                    }
-
-                    .application-div {
-                        display: flex;
-                        position: relative;
-                        flex-direction: column;
-                        align-items: center;
-                        height: 80px;
-                        width: 80px;
-                        margin: 5px;
-                        padding: 5px;
-                        border-radius: 5px;
-                        transition: background 0.2s ease,padding 0.8s linear;
-                    }
-
-                    .application-div img{
-                        filter: invert(0%);
-                    }
-                    .application-div:hover{
-                        cursor: pointer;
-                        -webkit-filter: invert(10%);
-                        filter: invert(10%);
-                    }
-
-                    .application-div img{
-                        height: 56px;
-                        width: 56px;
-                    }
-
-                    .application-div span{
-                        margin-top: 5px;
-                        color: #404040;
-                        display: inline-block;
-                        font-family: 'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;
-                        font-size: 14px;
-                        letter-spacing: .09px;
-                        line-height: 16px;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        white-space: nowrap;
-                        width: 76px;
-                        text-align: center;
-                        color: var(--palette-text-primary);
-                    }
-
-                </style>
                 <div id="${applicaiton._id}_div" class="application-div">
                     <paper-ripple recenters></paper-ripple>
                     <img id="${applicaiton._id}_img"></img>
                     <span id="${applicaiton._id}_span"></span>
                     <a id="${applicaiton._id}_lnk" style="display: none;"></a>
                 </div>
-            `
+                `
+                if (this.shadowRoot.querySelector(`#${applicaiton._id}_div`) == undefined) {
+                    this.shadowRoot.querySelector(".container").appendChild(range.createContextualFragment(html))
 
-                div.appendChild(range.createContextualFragment(html))
-                let div_ = this.shadowRoot.getElementById(applicaiton._id + "_div")
-                let img = this.shadowRoot.getElementById(applicaiton._id + "_img")
-                let lnk = this.shadowRoot.getElementById(applicaiton._id + "_lnk")
-                var currentLocation = window.location;
-                lnk.href = currentLocation.origin + applicaiton.path;
+                    let div_ = this.shadowRoot.getElementById(applicaiton._id + "_div")
+                    let img = this.shadowRoot.getElementById(applicaiton._id + "_img")
+                    let lnk = this.shadowRoot.getElementById(applicaiton._id + "_lnk")
+                    var currentLocation = window.location;
+                    lnk.href = currentLocation.origin + applicaiton.path;
 
-                let title = this.shadowRoot.getElementById(applicaiton._id + "_span")
-                img.src = applicaiton.icon;
-                title.innerHTML = applicaiton._id;
-                title.title = applicaiton._id;
-                
-                div_.onclick = ()=>{
-                    lnk.click()
-                }
-
-                // Keep the image up to date.
-                Application.eventHub.subscribe(`update_application_${applicaiton._id}_settings_evt`, 
-                (uuid)=>{
-        
-                }, 
-                (__applicationInfoStr__)=>{
-                    // Set the icon...
-                    let applicaiton = JSON.parse(__applicationInfoStr__)
+                    let title = this.shadowRoot.getElementById(applicaiton._id + "_span")
                     img.src = applicaiton.icon;
-                }, false)
+                    title.innerHTML = applicaiton._id;
+                    title.title = applicaiton._id;
 
+                    div_.onclick = () => {
+                        lnk.click()
+                    }
+
+                    // Keep the image up to date.
+                    Application.eventHub.subscribe(`update_application_${applicaiton._id}_settings_evt`,
+                        (uuid) => {
+
+                        },
+                        (__applicationInfoStr__) => {
+                            // Set the icon...
+                            let applicaiton = JSON.parse(__applicationInfoStr__)
+                            img.src = applicaiton.icon;
+                        }, false)
+
+                }
             }
-            this.shadowRoot.removeChild(this.getMenuDiv())
+
         }, (err) => {
             console.log(err)
         })
-        this.shadowRoot.removeChild(this.getMenuDiv())
     }
 }
 
-customElements.define('globular-applications-menu', ApplicationsMenu)
+customElements.define('globular-applications-panel', ApplicationsPanel)
