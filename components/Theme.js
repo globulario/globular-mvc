4439976b6eff1e7f9ea890eefe76f27dec604c11
davecourtois <dave.courtois60@gmail.com>
2021-11-08 21:42:02 -0500

    fix css

--- components/Theme.js
+++ components/Theme.js
@@ -313,22 +313,6 @@ export let theme = `
   .inner-triangle:hover{
       cursor: pointer;
   }
-  ::-webkit-scrollbar {
-    height: 12px;
-    width: 12px;
-    background: #000;
-  }
-
-  ::-webkit-scrollbar-thumb {
-      background: #393812;
-      -webkit-border-radius: 1ex;
-      -webkit-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.75);
-  }
-
-  ::-webkit-scrollbar-corner {
-      background: #000;
-  }
-
   emoji-picker {
     width: 400px;
     height: 300px;
