aac5755204eb9b223c5300b5adbaac27b997dd45
davecourtois <dave.courtois60@gmail.com>
2021-12-03 13:51:12 -0500

    ...

--- components/Console.js
+++ components/Console.js
@@ -24,8 +24,6 @@ export class Console extends HTMLElement {
         this.shadowRoot.innerHTML = `
          <style>
              ${theme}
-   
-
              paper-card{
                 position: relative;
                 display: flex;
