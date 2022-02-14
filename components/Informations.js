bb2c07462d258931ceef78857a077747c1aab914
davecourtois <dave.courtois60@gmail.com>
2022-02-13 19:05:02 -0500

    fix style on smal display

--- components/Informations.js
+++ components/Informations.js
@@ -198,7 +198,8 @@ export class InformationsManager extends HTMLElement {
 
                 .title-poster-div {
                     display: flex;
-                    align-items: center;
+                    justify-content: center;
+                    margin-bottom: 20px;
                 }
             }
 
